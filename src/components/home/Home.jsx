dfecb507c62caeb5fdd2459dcb599adbb395ecbf
anamulhaquemollah <anamul1414@gmail.com>
2023-02-03 12:32:14 +0600

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import MovieListing from "../movielisting/MovieListing"
+import React, { useEffect } from "react";
+import MovieListing from "../movielisting/MovieListing";
+import movieApi from "../../constant/apis/movieApi";
+import { APIKey } from "../../constant/apis/MovieApiKey";
 
 const Home = () => {
+  useEffect(() => {
+    let movieText = "harry"
+    const fetchMovies = async () => {
+      let response = await movieApi.get(
+        `?apikey=${APIKey}&s=${movieText}&type=movie`
+      )
+      .catch(err=>{
+        console.log("error", err)
+      })
+      console.log(response)
+    };
+    fetchMovies(); 
+  }, []);
+
   return (
     <>
-    <div className='banner-img'></div>
-    <MovieListing/>
+      <div className="banner-img"></div>
+      <MovieListing />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
